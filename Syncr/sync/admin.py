e28ba5ed8195c2169a45c111346f25e4f4e3701d
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-03-13 12:22:33 -0300

    Admin things

--- Syncr/sync/admin.py
+++ Syncr/sync/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Job)
+class jobAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'startTime', 'duration', 'endTime', 'error', 'finished', 'group', 'rcloneId', 'success', 'output', 'elapsedTime', 'bytes', 'checks', 'deletedDirs', 'deletes', 'errors', 'fatalError', 'renames', 'retryError', 'serverSideCopies', 'serverSideCopyBytes', 'serverSideMoveBytes', 'serverSideMoves', 'speed', 'totalBytes', 'totalChecks', 'totalTransfers', 'transferTime', 'transfers']
+    list_filter = ['user', 'startTime', 'endTime', 'finished', 'success', 'fatalError', 'retryError']
+
+admin.site.register(models.Job, jobAdmin)
