b317c46a5ba2548f1555745068a8512870de961f
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-03-17 20:15:50 -0300

    Check if the job was fucked up by the server

--- Syncr/sync/tasks.py
+++ Syncr/sync/tasks.py
@@ -1,12 +1,13 @@
 from huey import crontab
 from huey.contrib.djhuey import periodic_task
+import requests
 
 from cron_validator import CronValidator
 from datetime import datetime
-from .models import Task
+from .models import Job, Task
 
 @periodic_task(crontab(minute='*/1'))
-def every_min():
+def check_tasks_cron():
     tasks = Task.objects.all()
     dt = datetime.now()
     print(dt)
@@ -17,3 +18,31 @@ def every_min():
             print(f'Matched: {task.cron}')
         else:
             print(f'Unmatched: {task.cron}')
+
+
+# This task will check if I job has failed
+# It can fail when the rclone server gives up or loses connection
+# I'm unsure if this will work properly
+@periodic_task(crontab(minute="*/15"))
+def check_tasks_for_failure():
+    running_jobs = Job.objects.filter(finished=False)
+    for job in running_jobs:
+        try:
+            status = requests.post("http://127.0.0.1:5572/job/status", json={
+                "jobid": job.rcloneId
+            })
+            status.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            job.finished = True
+            job.success = False
+            job.error = f"Error contacting server: {str(e)}\nOur system was unable to query the job and was promptly finished"
+            job.save()
+            print(f"Job {job.rcloneId} failed due to server contact error")
+            continue
+        
+        if status.json().get("error"):
+            job.finished = True
+            job.success = False
+            job.error = status.json().get("error") + "\nOur system was unable to query the job and was promptly finished"
+            job.save()
+            print(f"Job {job.rcloneId} failed")
