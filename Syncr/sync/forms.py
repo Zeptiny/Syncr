8212e1c3b86c2fa09b3bd9ad25ed6b3d697c8b68
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-03-19 12:33:12 -0300

    Better form styling

--- Syncr/sync/forms.py
+++ Syncr/sync/forms.py
@@ -12,20 +12,33 @@ class remoteCreateForm(forms.ModelForm):
             'name': 'Remote Name',
             'config': 'Remote Configuration'
         }
+        
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}),
+            'type': forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}),
+        }
     def __init__(self, *args, **kwargs):
         super(remoteCreateForm, self).__init__(*args, **kwargs)
         self.fields['config'].widget = forms.HiddenInput()
 
 class jobCreateForm(forms.Form):
-    type = forms.ChoiceField(choices=
-        list(TASK_TYPES.items())
+    type = forms.ChoiceField(
+        choices=list(TASK_TYPES.items()),
+        widget=forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'})
         )
 
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request')
         super(jobCreateForm, self).__init__(*args, **kwargs)
-        self.fields['srcFs'] = forms.ModelChoiceField(queryset=models.Remote.objects.filter(user=self.request.user))
-        self.fields['dstFs'] = forms.ModelChoiceField(queryset=models.Remote.objects.filter(user=self.request.user))
+        
+        self.fields['srcFs'] = forms.ModelChoiceField(
+            queryset=models.Remote.objects.filter(user=self.request.user),
+            widget=forms.Select(attrs={'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'})
+        )
+        self.fields['dstFs'] = forms.ModelChoiceField(
+            queryset=models.Remote.objects.filter(user=self.request.user),
+            widget=forms.Select(attrs={'class': 'fbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'})
+        )
         
 
 class taskCreateForm(forms.ModelForm):
