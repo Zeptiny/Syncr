23057bb030a2552a12c917dc00aad739da9f915f
Zeptiny <arturtrentinibenedetti@gmail.com>
2025-04-01 15:57:27 -0300

    Better notification dropdown handling

--- Syncr/notifications/urls.py
+++ Syncr/notifications/urls.py
@@ -7,4 +7,7 @@ app_name = "notifications"
 urlpatterns = [
     path('', login_required(views.allNotificationsView.as_view()), name='notifications'),
     path('markAsRead/<int:notificationId>', login_required(views.markAsReadNotificationView.as_view()), name='markAsRead'),
+    
+    
+    path('dropdown/', login_required(views.notificationDropdown.as_view()), name='dropdown'),
 ]
--- Syncr/notifications/views.py
+++ Syncr/notifications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Notification
 from django.http import HttpResponse
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 class markAsReadNotificationView(View):
     def post(self, request, notificationId):
         Notification.objects.filter(id=notificationId, user=request.user).update(is_read=True)
-        return HttpResponse(content="", status=200) # Returning an empty response to replace the notification div with nothing
+        return redirect('notifications:dropdown') # Returning the dropdown to update
     
     
 class allNotificationsView(View):
@@ -15,3 +15,11 @@ class allNotificationsView(View):
             "notifications": Notification.objects.filter(user=request.user).order_by("-created_at"),
         }
         return render(request, "notifications/notifications.html", context)
+    
+    
+class notificationDropdown(View):
+    def get(self, request):
+        context = {
+            "notifications": Notification.objects.filter(user=request.user, is_read=False).order_by("-created_at")[:5],
+        }
+        return render(request, "notifications/notificationsDropdown.html", context)
